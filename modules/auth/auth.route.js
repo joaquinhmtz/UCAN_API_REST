cceec3dc23dffc5085a76a4efa3ee1836f45d917
Joaquín Hernández <joaquin_downhill@hotmail.com>
2025-06-09 15:04:17 -0600

    feat: integración y ajustes en inicio de sesión

--- modules/auth/auth.route.js
+++ modules/auth/auth.route.js
@@ -12,20 +12,23 @@ module.exports = (app, router) => {
                     let user = await UserLib.GetUser({ _id: account.userId });
 
                     if (user) {
+                        console.log("user**", user.profile.name)
                         let profile = await ProfileLib.GetProfileByName({ name: user.profile.name });
-                        if (!profile || profile === null) res.status(401).json({ message: "Lo sentimos, tu usuario no tiene asignado un perfil." });
-                        let token = jwt.sign(user.toJSON(), app.secret, { expiresIn : "1d" });
-                        //let configML = await AuthLib.GetConfigurationML();
-                        //let configAmz = await AuthLib.GetConfigurationAmz();
+                        if (!profile && profile === null) res.status(401).json({ message: "Lo sentimos, tu usuario no tiene asignado un perfil." });
+                        else {
+                            let token = jwt.sign(user.toJSON(), app.secret, { expiresIn : "1d" });
+                            //let configML = await AuthLib.GetConfigurationML();
+                            //let configAmz = await AuthLib.GetConfigurationAmz();
 
-                        return res.status(200).send({ 
-                            succes: true, 
-                            token: token, 
-                            user: user, 
-                            profile: { _id: profile._id, name: profile.name }, 
-                            //tokenML: configML.lastToken,
-                            //tokenAmz: configAmz.lastToken
-                        });
+                            return res.status(200).send({ 
+                                succes: true, 
+                                token: token, 
+                                user: user, 
+                                profile: { _id: profile._id, name: profile.name }, 
+                                //tokenML: configML.lastToken,
+                                //tokenAmz: configAmz.lastToken
+                            });
+                        }
 
                     } else res.status(401).json({ message: "No se encontró información del usuario" });
                 }
